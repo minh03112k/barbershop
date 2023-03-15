811c9eeafba847003100cd75d977c3da12f7810c
minh03112k <hoangminh03112k@gmail.com>
2023-03-16 01:40:51 +0700

    fix conflict

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import type { AppProps } from 'next/app';
-import '../styles/custom-bootstrap.css';
+import '../styles/custom-bootstrap.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
