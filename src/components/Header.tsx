cc260723ef14dbe2438932237f5371dcc131f721
minhnguyen4 <minhnguyen4@tdt.asia>
2023-03-17 17:23:32 +0700

    header menu item css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { colors, InputBase, TextField } from '@mui/material';
+import { Button, colors, InputBase, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -8,19 +8,45 @@ const Header = () => {
   const router = useRouter();
 
   return (
-    <Box  id="header" className={`${styles.header} bg-dark px-5`}>
+    <Box id="header" className={`${styles.header} bg-dark px-5`}>
       <Box className="d-flex align-items-center justify-content-between">
         <Box>Brand</Box>
-        <ul className="d-flex flex-row align-items-center m-0" style={{listStyleType: 'none'}}>
+        <ul className={`${styles.headerMenu}`}>
           {/* Todo: Custom Linl Component by Higher Order Function */}
-          <li className=''>
-            <Link className={router.asPath.includes('home') ? 'text-danger text-20' : 'text-20'} href={'#'}>Home</Link>
+          <li className="">
+            <Link className={router.asPath.includes('home') ? 'text-danger text-20' : 'text-20'} href={'#'}>
+              Home
+            </Link>
           </li>
-          <li className='mx-3'>
-            <Link className={router.asPath.includes('about') ? 'text-danger text-20' : 'text-20'} href={'#'}>About</Link>
+          <li className="">
+            <Link className={router.asPath.includes('book') ? 'text-danger text-20' : 'text-20'} href={'#'}>
+              Book
+            </Link>
           </li>
-          <li className=''>
-            <Link className={router.asPath.includes('Galleries') ? 'text-danger text-20' : 'text-20'} href={'#'}>Galleries</Link>
+          <li className="">
+            <Link className={router.asPath.includes('shop') ? 'text-danger text-20' : 'text-20'} href={'#'}>
+              Shop
+            </Link>
+          </li>
+          <li className="">
+            <Link className={router.asPath.includes('gallery') ? 'text-danger text-20' : 'text-20'} href={'#'}>
+              Gallery
+            </Link>
+          </li>
+          <li className="">
+            <Link className={router.asPath.includes('about') ? 'text-danger text-20' : 'text-20'} href={'#'}>
+              About
+            </Link>
+          </li>
+          <li className="">
+            <Button variant="contained">
+              <Typography>Login</Typography>
+            </Button>
+          </li>
+          <li>
+            <Button variant="outlined">
+              <Typography>Sign up</Typography>
+            </Button>
           </li>
         </ul>
       </Box>
